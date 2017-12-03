b03d98937b21935cbbcb34f3a7a158d8e3d2263e
Ramprits <Rampritsahani@yahoo.com>
2017-12-03 16:09:58 +0530

    Contact List Added

--- src/app/component/training/training.component.ts
+++ src/app/component/training/training.component.ts
@@ -3,6 +3,8 @@ import { TrackerError } from "../../core/tracker.error";
 import { Training } from "./training.module";
 import { TrainingService } from "./training.service";
 import { LoggerService } from "../../core/logging.service";
+import { Title } from "@angular/platform-browser";
+
 @Component({
   selector: "b-training",
   templateUrl: "./training.component.html"
@@ -13,8 +15,11 @@ export class TrainingComponent implements OnInit {
   @Input() training: Training;
   constructor(
     private trainingService: TrainingService,
-    private logger: LoggerService
-  ) {}
+    private logger: LoggerService,
+    private title: Title
+  ) {
+    this.title.setTitle("Training List");
+  }
 
   ngOnInit() {
     this.GetTrainings();
